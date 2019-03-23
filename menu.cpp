ca389d335b9cdee0cc06ce16ad94c8e4d932985b
jatin98batra <jatin98batra@gmail.com>
2019-03-23 23:43:00 +0530

    added Quit

--- menu.cpp
+++ menu.cpp
@@ -6,6 +6,8 @@ int menu()
 int choice;
 cout<<"--------------------------------------------Library Management System---------------------------------------"<<endl;
 
+
+cout<<"0.Quit"<<endl;
 cout<<"1.Enter New Book to the Database"<<endl;
 cout<<"2.Enter New Periodical to the Database"<<endl;
 cout<<"3.Enter New DVD to the Database"<<endl;
@@ -23,7 +25,7 @@ cout<<endl<<endl;
 while(1)
 {
 	cin>>choice;
-	if((choice>8)||(choice<1))
+	if((choice>8)||(choice<0))
 	{
 		cout<<"Wrong Choice"<<endl;
 		continue;
